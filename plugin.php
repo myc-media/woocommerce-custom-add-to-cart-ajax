a28c94b7599a4beb554867c9d2ae9f00b7a2e4ac
MYCGRAPHICS\web <web@mycgraphics.com>
2018-05-10 18:22:32 -0400

    updated user report table

--- plugin.php
+++ plugin.php
@@ -615,26 +615,47 @@ class MYCAjax{
     public static function myc_user_list_content(){
       echo '<h3>Users List</h3>';
 
-      function pumpUsers($users,$company){
-        echo '<table><thead><tr><th>User</th><th>Company</th></tr></thead><tbody>';
-        foreach($users as $user){
-          echo '<tr><td>' . $user->display_name . '</td><td>'.$company.'</td></tr>';
-        }
-        echo '</tbody></table>';
+      function pumpUsers($users){
+        ?>
+        <table>
+          <thead>
+            <tr>
+              <th>User</th>
+              <th>Esso</th>
+              <th>Mobil</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+              foreach($users as $user){
+                if($user->roles[0] == 'customer_on_account'){
+                  ?>
+                  <tr>
+                    <td><?php echo $user->display_name; ?></td>
+                    <td>&#10003;</td>
+                    <td></td>
+                  </tr>
+                  <?php
+                }elseif($user->roles[0] == 'mobile-customer-on-account'){
+                  ?>
+                  <tr>
+                    <td><?php echo $user->display_name; ?></td>
+                    <td></td>
+                    <td>&#10003;</td>
+                  </tr>
+                  <?php
+                }
+              }
+            ?>
+          </tbody>
+        </table>
+        <?php
       }
-      //'customer_on_account'
-      //'mobile-customer-on-account'
-      $essoUsers = get_users(array('role' => 'customer_on_account'));
-      $mobilUsers = get_users(array('role' => 'mobile-customer-on-account'));
-      echo '<div class="usersReport">';
-      echo '<div class="userReportSection">';
-      $renderEsso = pumpUsers($essoUsers, 'Esso');
-      echo '</div>';
-      echo '<div class="userReportSection">';
-      $renderMobil = pumpUsers($mobilUsers, 'Mobil');
-      echo '</div>';
-      echo '</div>';
+
+      $allUsers = get_users();
+      pumpUsers($allUsers);
     }
+
 
 
 
